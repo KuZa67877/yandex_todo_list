ce8125cca424a99270061ee6e157e2265c33690f
Egor Kuzmihykh <egor67877@gmail.com>
2024-07-11 16:15:38 +0300

    hot fix: integration test
    forget to add integration driver in commit

--- integration_test/add_task_test.dart
+++ integration_test/add_task_test.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:yandex_to_do_app/main.dart' as app;
 
 void main() {
-  group('Add Task Integration Test', () {
-    testWidgets('Add Task Test', (WidgetTester tester) async {
-      await tester.pumpWidget(const app.MyApp());
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-      await tester.pumpAndSettle();
+  testWidgets('Add task test', (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
 
-      await tester.tap(find.byKey(const ValueKey('add_task_button')));
-      await tester.pumpAndSettle();
+    final Finder addButton = find.byKey(const ValueKey('add_task_button'));
+    await tester.tap(addButton);
+    await tester.pumpAndSettle();
 
-      expect(find.byKey(const ValueKey("change_task_screen")), findsOneWidget);
+    final Finder textField = find.byKey(const ValueKey('text_field'));
+    const String taskText = 'New Task';
+    await tester.enterText(textField, taskText);
+    await tester.pumpAndSettle();
 
-      await tester.enterText(
-          find.byKey(const ValueKey("text_field")), 'Новая задача');
+    final Finder saveButton = find.byKey(const ValueKey('save_task'));
+    await tester.tap(saveButton);
+    await tester.pumpAndSettle();
 
-      await tester.tap(find.byKey(const ValueKey("save_task")));
-      await tester.pumpAndSettle();
-
-      expect(find.text('Новая задача'), findsOneWidget);
-    });
+    expect(find.text(taskText), findsOneWidget);
   });
 }
